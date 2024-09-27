d38345687c46acc08236f8068cd8ea4b12780523
amirnz026 <czhablog@gmail.com>
2024-09-27 18:02:16 +0330

    Backup

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,16 @@ import { App } from "@/App";
 import { ConfirmProvider } from "@/shared/confirm/components/ConfirmProvider";
 import { theme } from "@/shared/theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <ConfirmProvider>
-          <App />
-        </ConfirmProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <ConfirmProvider>
+        <App />
+      </ConfirmProvider>
+    </ThemeProvider>
   </StrictMode>
 );
